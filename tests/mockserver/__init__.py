7357b5533bbf6c6208403e25b7e1753491922be8
ryandaryl <ryandarylmills@gmail.com>
2018-02-22 09:54:17 +0000

    No commit message was set.

--- tests/mockserver/__init__.py
+++ tests/mockserver/__init__.py
@@ -6,6 +6,7 @@ google_app = Flask(__name__)
 @google_app.route("/tokeninfo")
 def validate():
     if request.args['id_token'] == 'XYZmyvalidtoken123':
-        return jsonify({'aud': '456testgoogleclientid789'})
+        return jsonify({'aud': '456testgoogleclientid789',
+                        'sub': '123456789012345678901'})
     else:
         return jsonify({'error_description': 'Invalid Value'})
