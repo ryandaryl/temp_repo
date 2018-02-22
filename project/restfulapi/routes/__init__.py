10c6b5240a40a7d907169232584a12a7d652f68b
ryandaryl <ryandarylmills@gmail.com>
2018-02-22 11:32:19 +0000

    Import app.

--- project/restfulapi/routes/__init__.py
+++ project/restfulapi/routes/__init__.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 import flask_restful
 from setuptools import find_packages
+from project import app
 import project.restfulapi.routes.route_decorator
 
 def get_blueprints(blueprints, filter_path=''):
