c2d95f9ad07e18cf9d65b7f1373f883a6b5edf99
ryandaryl <ryandarylmills@gmail.com>
2018-02-22 03:07:54 +0000

    tests.mockserver

--- tests/scripts/tests.sh
+++ tests/scripts/tests.sh
@@ -1,5 +1,5 @@
 #Start mock server for 'https://www.google.com'
 echo $PWD
-(python run_server.py --package mockserver --app google_app --port 8001 &)
+(coverage run --rcfile=tests/.coveragerc run_server.py --package tests.mockserver --app google_app --port 8001 &)
 
 source tests/scripts/behave.sh
