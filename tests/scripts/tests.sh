f21c4f3571b4a8a9380564adcafe9179fbdf528b
ryandaryl <ryandarylmills@gmail.com>
2018-02-22 03:20:39 +0000

    Omit "tests" dir from coverage.

--- tests/scripts/tests.sh
+++ tests/scripts/tests.sh
@@ -1,5 +1,4 @@
 #Start mock server for 'https://www.google.com'
-echo $PWD
-(coverage run --rcfile=tests/.coveragerc run_server.py --package tests.mockserver --app google_app --port 8001 &)
+(python run_server.py --package tests.mockserver --app google_app --port 8001 &)
 
 source tests/scripts/behave.sh
