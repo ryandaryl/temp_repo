2814b99e3cea14ad33c3a9b45a5b9d8d0900e50f
ryandaryl <ryandarylmills@gmail.com>
2018-02-22 02:56:49 +0000

    Try to fix it.

--- tests/scripts/tests.sh
+++ tests/scripts/tests.sh
@@ -1,5 +1,4 @@
 #Start mock server for 'https://www.google.com'
-cd ../..
-(run_server.py --package tests.mockserver --app google_app --port 8001 &)
+(../run_server.py --package mockserver --app google_app --port 8001 &)
 
 source tests/scripts/behave.sh
