eb4d82a7b024d6f0b30372d8932953cbcc60fcda
ryandaryl <ryandarylmills@gmail.com>
2018-02-23 11:08:00 +0000

    Subclass Login 8.

--- project/restfulapi/routes/__init__.py
+++ project/restfulapi/routes/__init__.py
@@ -5,8 +5,12 @@ from project import app
 import project.restfulapi.routes.route_decorator
 
 def get_blueprints(blueprints, filter_path=''):
+    paths = []
     for path in find_packages():
         if filter_path in path:
-            blueprints[path] = Blueprint(path, path)
-            __import__(path)
-            app.register_blueprint(blueprints[path])
+            paths.append(path)
+    for path in paths:
+        blueprints[path] = Blueprint(path, path)
+    for path in paths:
+        __import__(path)
+        app.register_blueprint(blueprints[path])
