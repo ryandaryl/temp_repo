9d8c49c876ec86db63cef34c9bdc13065a32a7d9
ryandaryl <ryandarylmills@gmail.com>
2018-02-23 10:35:43 +0000

    Subclass Login 2.

--- project/restfulapi/session/__init__.py
+++ project/restfulapi/session/__init__.py
@@ -1,13 +1,5 @@
-from flask_login import LoginManager, UserMixin
+from flask_login import LoginManager
 from project import app
-
-class User(UserMixin):
-
-    def __init__(self, id):
-        self.id = id
-        
-    def __repr__(self):
-        return str(self.id)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
--- project/restfulapi/session/flask_session.py
+++ project/restfulapi/session/flask_session.py
@@ -1,10 +1,20 @@
 from flask import jsonify, request
 import flask_restful
-from flask_login import login_required, logout_user, User
+from flask_login import login_required, logout_user, UserMixin
 from project.restfulapi import blueprints
 
 api = flask_restful.Api(blueprints[__package__], prefix="/session")
 
+
+#This should be in Models
+class User(UserMixin):
+
+    def __init__(self, id):
+        self.id = id
+        
+    def __repr__(self):
+        return str(self.id)
+        
 
 @api.route('/')
 class SimpleLogout(flask_restful.Resource):
